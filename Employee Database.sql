ef3aecd68a1bf0de3ad969f336a1200a7c5623ed
AbhishekSherkar <168963266+AbhishekSherkar@users.noreply.github.com>
2024-05-06 00:05:09 +0530

    Add files via upload

---
+++ Employee Database.sql
@@ -0,0 +1,11 @@
+create database sqlsessions;
+
+use sqlsessions ;
+
+CREATE TABLE Employee (
+    PersonID int,
+    Name varchar(255),
+    Department varchar(255),
+    Salary int,
+    Age int
+);
